550c6fab75960fb42c0c76ad806d5866c0ba6029
Mickael Lavroff <mickael.lavroff@nebiatek.com>
2023-08-15 21:57:09 -0400

    first version of powershell function to disable shared mailbox user accounts

---
+++ Disable-SharedMailboxUserAccount.ps1
@@ -0,0 +1,61 @@
+#Requires -Modules Microsoft.Graph.Authentication, Microsoft.Graph.Users, ExchangeOnlineManagement
+
+function Disable-SharedMailboxUserAccount {
+
+    [CmdletBinding(DefaultParameterSetName = 'AllMailboxes')]
+    
+    param (
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline,
+            ValueFromPipelineByPropertyName)]
+        [string]
+        $TenantId,
+
+        [Parameter(Mandatory = $false,
+            ParameterSetName = 'AllSharedMailboxes')]
+        [switch]
+        $All,
+
+        # TODO: Allow to call this function providing an array of one or more userprincipalname of sharedmailbox to disable
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'TargetedSharedMailboxes')]
+        [string[]]
+        $Identity,
+
+        # TODO: Require manual confirmation before each "disable account" action
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $Confirm
+    )
+    
+    begin {
+        Connect-MgGraph -TenantId $TenantId -Scopes Directory.ReadWrite.All
+        Connect-ExchangeOnline -ShowBanner:$false
+        $disabledaccounts = @()
+    }
+    
+    process {
+        $sharedmailboxes = Get-EXOMailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
+
+        for ($i = 0; $i -lt $sharedmailboxes.Count; $i++) {
+
+            try {
+                $user = Get-MgUser -Filter "UserPrincipalName eq '$($($sharedmailboxes[$i].UserPrincipalName))'"
+            }
+            catch {
+                Write-Error $_.Exception.Message
+            }
+            
+            Write-Verbose "Disabling $($user.UserPrincipalName) with object id $($user.Id)"
+            Update-MgUser -UserId $user.Id -BodyParameter @{AccountEnabled = "false"}
+
+            $disabledaccounts += ($user | Select-Object Id,UserPrincipalName)
+        }
+    }
+    
+    end {
+        return $disabledaccounts
+        Disconnect-MgGraph | Out-Null
+        Disconnect-ExchangeOnline | Out-Null
+    }
+}
