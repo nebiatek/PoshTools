fc082e93123c4bdfd0261bf4aa56148ac5d2f933
mickael-lavroff <93455834+mickael-lavroff@users.noreply.github.com>
2026-02-26 08:00:39 -0500

    Add script to configure FSLogix for Azure Files
    This script configures FSLogix settings for Azure Files and enables Kerberos ticket retrieval.

---
+++ Set-CloudNativeFSLogix.ps1
@@ -0,0 +1,14 @@
+# Code used for AVD and FSLogix with Entra-only Azure Files https://youtu.be/SHHP2ZoFBD4
+
+# Configure the clients to retrieve Kerberos tickets
+# https://learn.microsoft.com/en-us/azure/storage/files/storage-files-identity-auth-hybrid-identities-enable?tabs=azure-portal%2Cregkey&WT.mc_id=AZ-MVP-5004159#configure-the-clients-to-retrieve-kerberos-tickets
+reg add HKLM\SYSTEM\CurrentControlSet\Control\Lsa\Kerberos\Parameters /v CloudKerberosTicketRetrievalEnabled /t REG_DWORD /d 1
+
+
+# Registry Settings for FSLogix
+$profilePath = "\\<storage_accountname>.file.core.windows.net\<share>\<directory>"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profilePath
+Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 30000 -Type DWord
+Set-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -Type DWord
